6fe94824a296891da1156d5bf396ca51f31e59ed
ak1t0 <aktoo3097@gmail.com>
2018-10-20 17:27:25 +0900

    rebuild by package.el and use-package

--- init.el
+++ init.el
@@ -1,132 +1,191 @@
-;;;; 基本設定
+;; Init
+;;; package.el
+(require 'package)
+(package-initialize)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+	("melpa" . "http://melpa.org/packages/")
+	("org" . "http://orgmode.org/elpa/")))
+(unless package-archive-contents
+    (package-refresh-contents))
 
-;; ウィンドウの初期設定 
-(setq package--init-file-ensured t)
+;;; use-package
+(when (not (package-installed-p 'use-package))
+  (package-install 'use-package))
+(require 'use-package)
+
+
+;; General
+;;; system
+;;;; message
 (setq initial-scratch-message nil)
 (setq inhibit-startup-message t)
-(tool-bar-mode 0)
+;;;; cutom
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
-;; テーマを設定
-(load-theme 'misterioso t)
-
-;; 読み込んだ設定ファイルのディレクトリを変数に設定
-(when load-file-name
-  (setq user-emacs-directory (file-name-directory load-file-name)))
-
-;;;; El-Getの設定
-
-;; EmacsのPATHに$PATH/el-get/el-getを追加 
-(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
-
-;; El-Getが存在しないときインストールする
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-;;; General
-(el-get-bundle dash)
-(el-get-bundle flycheck)
-(el-get-bundle company-mode)
-(el-get-bundle xclip)
-
-(ffap-bindings)
-(show-paren-mode 1)
-(setq frame-title-format
-      (format "%%f"))
+;;; UI/UX
+;;;; final newline
 (setq require-final-newline t)
-(set-language-environment "UTF-8")
-(turn-on-xclip)
-(xterm-mouse-mode 1)
+;;;; paren
+(show-paren-mode t)
+(setq show-paren-style 'mixed)
+;;;; bar mode
+(tool-bar-mode 0)
+(menu-bar-mode 0)
 
-;; key-binding
+;;; key
 (define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
-(define-key global-map (kbd "C-x g") 'helm-keyboard-quit)
-(define-key key-translation-map (kbd "C-j") (kbd "C-g"))
+(define-key key-translation-map (kbd "C-z") (kbd "C-g"))
 
-;; for Custom warning
-(load (setq custom-file (expand-file-name "custom.el" user-emacs-directory)))
+
+;; Package
+;;; General
+;;;; xclip
+(use-package xclip
+  :ensure t
+  :config
+  (xclip-mode t)
+  (xterm-mouse-mode t))
+;;;; flycheck
+(use-package flycheck
+  :ensure t)
+;;;; company
+(use-package company
+  :ensure t
+  :config
+  (custom-set-faces
+   '(company-preview
+     ((t (:foreground "darkgray" :underline t))))
+   '(company-preview-common
+     ((t (:inherit company-preview))))
+   '(company-tooltip
+     ((t (:background "lightgray" :foreground "black"))))
+   '(company-tooltip-selection
+     ((t (:background "steelblue" :foreground "white"))))
+   '(company-tooltip-common
+     ((((type x)) (:inherit company-tooltip :weight bold))
+      (t (:inherit company-tooltip))))
+   '(company-tooltip-common-selection
+     ((((type x)) (:inherit company-tooltip-selection :weight bold))
+            (t (:inherit company-tooltip-selection))))))
 
 ;;; Helm
-(el-get-bundle helm)
-;; change key
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
-(global-unset-key (kbd "C-x c"))
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-c h o") 'helm-occur)
+;;;; helm
+(use-package helm
+  :ensure t
+  :config
+  (helm-mode t)
+  (global-set-key (kbd "C-x b") 'helm-mini)
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  (global-set-key (kbd "M-x") 'helm-M-x)
+  (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+  (setq helm-split-window-in-side-p           t
+	helm-move-to-line-cycle-in-source     t
+	helm-ff-search-library-in-sexp        t
+	helm-scroll-amount                    8
+	helm-ff-file-name-history-use-recentf t
+	helm-echo-input-in-header-line t
+	helm-autoresize-mode t)
+  (setq helm-buffers-fuzzy-matching t
+	helm-recentf-fuzzy-match    t))
 
-(setq helm-split-window-in-side-p           t
-      helm-move-to-line-cycle-in-source     t
-      helm-ff-search-library-in-sexp        t
-      helm-scroll-amount                    8
-      helm-ff-file-name-history-use-recentf t
-      helm-echo-input-in-header-line t
-      helm-autoresize-mode t)
-(setq helm-buffers-fuzzy-matching t
-      helm-recentf-fuzzy-match    t)
-(helm-mode 1)
-
-;;; Go
-;; go get -u github.com/rogpeppe/godef
-;; go get -u github.com/golang/lint/
-;; go get -u github.com/nsf/gocode
-(el-get-bundle go-mode)
-(el-get-bundle go-lint)
-(el-get-bundle company-go)
-(el-get-bundle flycheck-gometalinter)
-
-(add-to-list 'exec-path (expand-file-name "~/go/bin"))
-
-(add-hook 'go-mode-hook
-	  (lambda ()
-	    (set (make-local-variable 'company-backends) '(company-go))
-	    (company-mode)))
-(add-hook 'go-mode-hook
-	  (lambda ()
-	    (local-set-key (kbd "M-.") 'godef-jump)
-	    (local-set-key (kbd "M-,") 'pop-tag-mark)))
-
-(flycheck-gometalinter-setup)
-(setq flycheck-gometalinter-fast t)
-(setq flycheck-gometalinter-test t)
-
-(add-hook 'go-mode-hook 'flycheck-mode)
-(add-hook 'before-save-hook 'gofmt-before-save)
+;;; Golang
+;;;- go get -u github.com/rogpeppe/godef
+;;;- go get -u github.com/nsf/gocode
+;;;- go get -u github.com/alecthomas/gometalinter
+;;;; go-mode
+(use-package go-mode
+  :ensure t
+  :init
+  (add-hook 'go-mode-hook 'flycheck-mode)
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  :config
+  (define-key go-mode-map (kbd "M-.") 'godef-jump)
+  (define-key go-mode-map (kbd "M-,") 'pop-tag-mark))
+;;;; gometalinter
+(use-package flycheck-gometalinter
+  :ensure t
+  :config
+  (flycheck-gometalinter-setup)
+  (setq flycheck-gometalinter-fast t)
+  (setq flycheck-gometalinter-test t))
+;;;; company-go
+(use-package company-go
+  :ensure t
+  :config
+  (add-hook 'go-mode-hook 'company-mode)
+  (add-hook 'go-mode-hook
+	    (lambda ()
+	      (set (make-local-variable 'company-backends) '(company-go))
+	      (setq company-transformers '(company-sort-by-backend-importance))
+	      (setq company-selection-wrap-around t)
+	      (define-key company-active-map (kbd "C-n") 'company-select-next)
+	      (define-key company-active-map (kbd "C-p") 'company-select-previous)
+	      (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
+	      (define-key company-active-map (kbd "C-x") 'company-abort)
+	      (define-key company-active-map (kbd "C-j") 'company-complete-selection))))
 
 ;;; Rust
-(el-get-bundle rust-mode)
-(el-get-bundle flycheck-rust)
-(el-get-bundle racer)
-
-(add-to-list 'exec-path (expand-file-name "~/.cargo/bin/"))
-
-(add-hook 'rust-mode-hook #'flycheck-mode)
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'company-mode)
-
-;;; Markdown
-(el-get-bundle markdown-mode)
-
-;;; Ruby
-(el-get-bundle ruby-mode)
-(el-get-bundle ruby-electric)
-
-(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
-
-;;; Docker
-(el-get-bundle dockerfile-mode)
-
-;;; Clojure
-(el-get-bundle clojure-mode)
-(el-get-bundle cider)
-
-(add-hook 'cider-mode-hook #'company-mode)
+;;;; rust-mode
+(use-package rust-mode
+  :ensure t
+  :config
+  (add-hook 'rust-mode-hook 'flycheck-mode))
+;;;; racer
+(use-package racer
+  :ensure t
+  :config
+  (add-hook 'rust-mode-hook 'racer-mode)
+  (add-hook 'rust-mode-hook 'company-mode)
+  (add-hook 'rust-mode-hook
+	    (lambda ()
+	      (define-key company-active-map (kbd "C-n") 'company-select-next)
+	      (define-key company-active-map (kbd "C-p") 'company-select-previous)
+	      (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
+	      (define-key company-active-map (kbd "C-x") 'company-abort)
+	      (define-key company-active-map (kbd "C-j") 'company-complete-selection))))
+;;;; flycheck-rust
+(use-package flycheck-rust
+  :ensure t
+  :config
+  (add-hook 'rust-mode-hook 'flycheck-rust-setup))
 
 ;;; Haskell
-(el-get-bundle haskell-mode)
-(el-get-bundle intero)
-(add-hook 'haskell-mode-hook 'intero-mode)
+;;;; haskell-mode
+(use-package haskell-mode
+  :ensure t)
+;;;; intero
+(use-package intero
+  :ensure t
+  :config
+  (add-hook 'haskell-mode-hook 'intero-mode))
+
+;;; Ruby
+;;;; ruby-mode
+(use-package ruby-mode
+  :ensure t)
+;;;; ruby-electric
+(use-package ruby-electric
+  :ensure t
+  :config
+  (add-hook 'ruby-mode-hook
+	    (lambda ()
+	      (ruby-electric-mode t))))
+
+;;; Clojure
+;;;; clojure-mode
+(use-package clojure-mode
+  :ensure t)
+;;;; cider
+(use-package cider
+  :ensure t)
+
+;;; Docker
+;;;; dockerfile-mode
+(use-package dockerfile-mode
+  :ensure t)
+
+;;; Markdown
+;;;; markdown-mode
+(use-package markdown-mode
+  :ensure t)
