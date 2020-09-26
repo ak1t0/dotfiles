ca17a470bce757b9dc3964913f1ca0372c259b9b
ak1t0 <aktoo3097@gmail.com>
2020-09-26 23:42:10 +0900

    fix logic

--- dotfiles/pull.sh
+++ dotfiles/pull.sh
@@ -4,7 +4,7 @@
 cp ~/.gitconfig ./git/.gitconfig
 cp ~/.gitignore ./git/.gitignore
 # Emacs
-cp ~/.emacs.d/init.el ./init.el
+cp ~/.emacs.d/init.el ./emacs/init.el
 cp ~/.emacs.d/emacs/*.el ./emacs/emacs
 # tmux
 cp ~/.tmux.conf ./tmux/.tmux.conf
