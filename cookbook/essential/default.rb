cd95c2faf97da8b93073b3cff7f9d85eb4e08681
ak1t0 <aktoo3097@gmail.com>
2020-09-29 00:31:16 +0900

    improve package position

--- cookbook/essential/default.rb
+++ cookbook/essential/default.rb
@@ -1,4 +1,22 @@
-node[:packages].each do |p|
+packages = [
+  "emacs",
+  "git",
+  "zsh",
+  "tmux",
+  "curl",
+  "binutils",
+  "build-essential",
+  "gconf2",
+  "xclip",
+  "pkg-config",
+  "libssl-dev",
+  "cmake",
+  "exuberant-ctags",
+  "texinfo",
+  "zlib1g-dev"
+]
+
+packages.each do |p|
   package p do
     user "root"
     action :install
