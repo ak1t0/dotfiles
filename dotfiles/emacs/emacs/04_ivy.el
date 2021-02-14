918915b6bd816b7d26c7511070f2464d28358eff
ak1t0 <aktoo3097@gmail.com>
2021-02-14 16:13:09 +0900

    improve ivy

--- dotfiles/emacs/emacs/04_ivy.el
+++ dotfiles/emacs/emacs/04_ivy.el
@@ -7,6 +7,8 @@
   ;; general
   (setq ivy-use-virtual-buffers t)
   (setq enable-recursive-minibuffers t)
+  (setq ivy-wrap t)
+  (setq ivy-use-virtual-buffers t)
   ;; binding
   (global-set-key "\C-s" 'swiper)
   (global-set-key (kbd "M-x") 'counsel-M-x)
